f10a04efc89d6f1e23ca20b2f4b65bd207f29199
Konstantin <ivanovggxf@gmail.com>
2024-02-08 17:27:36 +0400

    11

--- themes/heo/components/MenuListTop.js
+++ themes/heo/components/MenuListTop.js
@@ -53,7 +53,7 @@ export const MenuListTop = () => {
       tagItems: [],
       password: '',
       show: true
-    }
+    },
     // {
     //   title: 'Sites',
     //   name: 'Sites',
@@ -195,179 +195,179 @@ export const MenuListTop = () => {
     //     }
     //   ]
     // },
-    // {
-    //   id: '9560538f-e8aa-4979-ba6e-f896b4be685a',
-    //   icon: 'fas fa-info',
-    //   date: {
-    //     start_date: '2023-03-23'
-    //   },
-    //   type: 'Menu',
-    //   slug: 'https://www.youtube.com/netsec',
-    //   summary: '普通菜单 跳转到指定路径',
-    //   title: 'About Me',
-    //   status: 'Published',
-    //   publishDate: 1679529600000,
-    //   publishTime: 'Mar 23, 2023',
-    //   lastEditedTime: 'Jul 25, 2023',
-    //   fullWidth: false,
-    //   pageIcon: '',
-    //   pageCover: '',
-    //   pageCoverThumbnail: '',
-    //   tagItems: [],
-    //   to: 'https://www.youtube.com/netsec',
-    //   name: 'About Me',
-    //   password: '',
-    //   show: true,
-    //   target: '_blank'
-    // },
-    // {
-    //   id: 'd81ce488-c4ba-4924-8b3f-a22b087abf94',
-    //   icon: 'fas fa-search',
-    //   date: {
-    //     start_date: '2021-07-02'
-    //   },
-    //   type: 'Menu',
-    //   slug: '/search',
-    //   tags: ['Text'],
-    //   summary: '跳转到’/search’搜索页的菜单',
-    //   title: 'Search',
-    //   status: 'Published',
-    //   publishDate: 1625184000000,
-    //   publishTime: 'Jul 2, 2021',
-    //   lastEditedTime: 'Jul 24, 2023',
-    //   fullWidth: false,
-    //   pageIcon: '🗒️',
-    //   pageCover: '',
-    //   pageCoverThumbnail: '',
-    //   tagItems: [
-    //     {
-    //       name: 'Text',
-    //       color: 'brown'
-    //     }
-    //   ],
-    //   to: '/search',
-    //   name: 'Search',
-    //   password: '',
-    //   show: true
-    // },
-    // {
-    //   id: 'ea934ee5-8341-4a49-845f-cfb8f333f8fb',
-    //   icon: 'fa-regular fa-comments',
-    //   date: {
-    //     start_date: '2020-07-01'
-    //   },
-    //   type: 'Menu',
-    //   slug: '/message',
-    //   summary:
-    //     '更多帮助请访问手册 → https://tangly1024.com/article/notion-next-secondary-menu',
-    //   title: 'Guestbook',
-    //   status: 'Published',
-    //   publishDate: 1593561600000,
-    //   publishTime: 'Jul 1, 2020',
-    //   lastEditedTime: 'Jul 25, 2023',
-    //   fullWidth: false,
-    //   pageIcon: '🗒️',
-    //   pageCover: 'https://source.unsplash.com/random/720x480/?encryption',
-    //   pageCoverThumbnail:
-    //     'https://source.unsplash.com/random/720x480/?encryption',
-    //   tagItems: [],
-    //   to: '/message',
-    //   name: 'Guestbook',
-    //   password: '',
-    //   show: true
-    // },
-    // {
-    //   id: '7ac7ea58-4cbd-4fc8-8463-31c97b7a3b1e',
-    //   icon: 'fas fa-archive',
-    //   date: {
-    //     start_date: '2020-07-01'
-    //   },
-    //   type: 'Menu',
-    //   slug: '#',
-    //   title: 'Others',
-    //   status: 'Published',
-    //   publishDate: 1593561600000,
-    //   publishTime: 'Jul 1, 2020',
-    //   lastEditedTime: 'Jul 25, 2023',
-    //   fullWidth: false,
-    //   pageIcon: '',
-    //   pageCover: '',
-    //   pageCoverThumbnail: '',
-    //   tagItems: [],
-    //   to: '#',
-    //   name: 'Others',
-    //   password: '',
-    //   show: true,
-    //   subMenus: [
-    //     {
-    //       id: '5c0bf0b4-6d8b-4660-8d82-770e364878d3',
-    //       icon: 'fas fa-clock-rotate-left',
-    //       date: {
-    //         start_date: '2020-07-01'
-    //       },
-    //       type: 'SubMenu',
-    //       slug: '/archive',
-    //       title: 'Archives',
-    //       status: 'Published',
-    //       publishDate: 1593561600000,
-    //       publishTime: 'Jul 1, 2020',
-    //       lastEditedTime: 'Jul 25, 2023',
-    //       fullWidth: false,
-    //       pageIcon: '🗒️',
-    //       pageCover: '',
-    //       pageCoverThumbnail: '',
-    //       tagItems: [],
-    //       to: '/archive',
-    //       name: 'Archives',
-    //       password: '',
-    //       show: true
-    //     },
-    //     {
-    //       id: '83088825-d84c-4045-b4ec-6c9213363867',
-    //       icon: 'fas fa-th',
-    //       date: {
-    //         start_date: '2020-07-01'
-    //       },
-    //       type: 'SubMenu',
-    //       slug: '/category',
-    //       title: 'Categories',
-    //       status: 'Published',
-    //       publishDate: 1593561600000,
-    //       publishTime: 'Jul 1, 2020',
-    //       lastEditedTime: 'Jul 25, 2023',
-    //       fullWidth: false,
-    //       pageIcon: '🗒️',
-    //       pageCover: '',
-    //       pageCoverThumbnail: '',
-    //       tagItems: [],
-    //       to: '/category',
-    //       name: 'Categories',
-    //       password: '',
-    //       show: true
-    //     },
-    //     {
-    //       id: '3fde6abb-9bb1-4924-8ac9-b79b458567f8',
-    //       icon: 'fas fa-tag',
-    //       date: {
-    //         start_date: '2020-07-01'
-    //       },
-    //       type: 'SubMenu',
-    //       slug: '/tag',
-    //       title: 'Tags',
-    //       status: 'Published',
-    //       publishDate: 1593561600000,
-    //       publishTime: 'Jul 1, 2020',
-    //       lastEditedTime: 'Jul 25, 2023',
-    //       fullWidth: false,
-    //       pageIcon: '🗒️',
-    //       pageCover: '',
-    //       pageCoverThumbnail: '',
-    //       tagItems: [],
-    //       to: '/tag',
-    //       name: 'Tags',
-    //       password: '',
-    //       show: true
-    //     }
+    {
+      id: '9560538f-e8aa-4979-ba6e-f896b4be685a',
+      icon: 'fas fa-info',
+      date: {
+        start_date: '2023-03-23'
+      },
+      type: 'Menu',
+      slug: 'https://www.youtube.com/netsec',
+      summary: '普通菜单 跳转到指定路径',
+      title: 'About Me',
+      status: 'Published',
+      publishDate: 1679529600000,
+      publishTime: 'Mar 23, 2023',
+      lastEditedTime: 'Jul 25, 2023',
+      fullWidth: false,
+      pageIcon: '',
+      pageCover: '',
+      pageCoverThumbnail: '',
+      tagItems: [],
+      to: 'https://www.youtube.com/netsec',
+      name: 'About Me',
+      password: '',
+      show: true,
+      target: '_blank'
+    },
+    {
+      id: 'd81ce488-c4ba-4924-8b3f-a22b087abf94',
+      icon: 'fas fa-search',
+      date: {
+        start_date: '2021-07-02'
+      },
+      type: 'Menu',
+      slug: '/search',
+      tags: ['Text'],
+      summary: '跳转到’/search’搜索页的菜单',
+      title: 'Search',
+      status: 'Published',
+      publishDate: 1625184000000,
+      publishTime: 'Jul 2, 2021',
+      lastEditedTime: 'Jul 24, 2023',
+      fullWidth: false,
+      pageIcon: '🗒️',
+      pageCover: '',
+      pageCoverThumbnail: '',
+      tagItems: [
+        {
+          name: 'Text',
+          color: 'brown'
+        }
+      ],
+      to: '/search',
+      name: 'Search',
+      password: '',
+      show: true
+    },
+    {
+      id: 'ea934ee5-8341-4a49-845f-cfb8f333f8fb',
+      icon: 'fa-regular fa-comments',
+      date: {
+        start_date: '2020-07-01'
+      },
+      type: 'Menu',
+      slug: '/message',
+      summary:
+        '更多帮助请访问手册 → https://tangly1024.com/article/notion-next-secondary-menu',
+      title: 'Guestbook',
+      status: 'Published',
+      publishDate: 1593561600000,
+      publishTime: 'Jul 1, 2020',
+      lastEditedTime: 'Jul 25, 2023',
+      fullWidth: false,
+      pageIcon: '🗒️',
+      pageCover: 'https://source.unsplash.com/random/720x480/?encryption',
+      pageCoverThumbnail:
+        'https://source.unsplash.com/random/720x480/?encryption',
+      tagItems: [],
+      to: '/message',
+      name: 'Guestbook',
+      password: '',
+      show: true
+    },
+    {
+      id: '7ac7ea58-4cbd-4fc8-8463-31c97b7a3b1e',
+      icon: 'fas fa-archive',
+      date: {
+        start_date: '2020-07-01'
+      },
+      type: 'Menu',
+      slug: '#',
+      title: 'Others',
+      status: 'Published',
+      publishDate: 1593561600000,
+      publishTime: 'Jul 1, 2020',
+      lastEditedTime: 'Jul 25, 2023',
+      fullWidth: false,
+      pageIcon: '',
+      pageCover: '',
+      pageCoverThumbnail: '',
+      tagItems: [],
+      to: '#',
+      name: 'Others',
+      password: '',
+      show: true,
+      subMenus: [
+        {
+          id: '5c0bf0b4-6d8b-4660-8d82-770e364878d3',
+          icon: 'fas fa-clock-rotate-left',
+          date: {
+            start_date: '2020-07-01'
+          },
+          type: 'SubMenu',
+          slug: '/archive',
+          title: 'Archives',
+          status: 'Published',
+          publishDate: 1593561600000,
+          publishTime: 'Jul 1, 2020',
+          lastEditedTime: 'Jul 25, 2023',
+          fullWidth: false,
+          pageIcon: '🗒️',
+          pageCover: '',
+          pageCoverThumbnail: '',
+          tagItems: [],
+          to: '/archive',
+          name: 'Archives',
+          password: '',
+          show: true
+        },
+        {
+          id: '83088825-d84c-4045-b4ec-6c9213363867',
+          icon: 'fas fa-th',
+          date: {
+            start_date: '2020-07-01'
+          },
+          type: 'SubMenu',
+          slug: '/category',
+          title: 'Categories',
+          status: 'Published',
+          publishDate: 1593561600000,
+          publishTime: 'Jul 1, 2020',
+          lastEditedTime: 'Jul 25, 2023',
+          fullWidth: false,
+          pageIcon: '🗒️',
+          pageCover: '',
+          pageCoverThumbnail: '',
+          tagItems: [],
+          to: '/category',
+          name: 'Categories',
+          password: '',
+          show: true
+        },
+        {
+          id: '3fde6abb-9bb1-4924-8ac9-b79b458567f8',
+          icon: 'fas fa-tag',
+          date: {
+            start_date: '2020-07-01'
+          },
+          type: 'SubMenu',
+          slug: '/tag',
+          title: 'Tags',
+          status: 'Published',
+          publishDate: 1593561600000,
+          publishTime: 'Jul 1, 2020',
+          lastEditedTime: 'Jul 25, 2023',
+          fullWidth: false,
+          pageIcon: '🗒️',
+          pageCover: '',
+          pageCoverThumbnail: '',
+          tagItems: [],
+          to: '/tag',
+          name: 'Tags',
+          password: '',
+          show: true
+        }
       ]
     }
   ]
